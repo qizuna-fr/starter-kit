d09859f6f8427da5bb1da60abb90b8adadf4a501
Mathieu <mathieu@qizuna.fr>
2023-01-28 09:25:08 +0100

    Starter Kit - admin logo - dummy menus

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use Symfony\Component\Asset\VersionStrategy\JsonManifestVersionStrategy;
+use Symfony\Component\Filesystem\Path;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Asset\Package;
+
+class DashboardController extends AbstractDashboardController
+{
+
+
+    public function __construct(private AdminUrlGenerator $urlGenerator)
+    {
+    }
+
+    #[Route('/admin', name: 'admin')]
+    public function index(): Response
+    {
+        return $this->redirect($this->urlGenerator->setController(UserCrudController::class)->generateUrl());
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        $package = new Package(
+            new JsonManifestVersionStrategy(Path::join($this->getParameter('kernel.project_dir'), "public/build/manifest.json"))
+        );
+
+        $image = $package->getUrl("build/images/qizuna.png");
+        $favicon = $package->getUrl("build/images/favicon.png");
+
+        return Dashboard::new()
+            ->renderContentMaximized()
+            ->setTitle('<img src="' . $image . '">')
+            ->setFaviconPath($favicon);
+
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        //yield MenuItem::linkToDashboard('Dashboard', 'fa fa-dashboard');
+        yield MenuItem::linkToCrud('Utilisateurs' , 'fa fa-users', User::class);
+        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+    }
+}
