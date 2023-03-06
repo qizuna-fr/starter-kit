7a5dff035378ade99110c6ddba6fa6f2b8c0328a
Mathieu <mathieu@qizuna.fr>
2023-03-06 11:23:54 +0100

    Add Sentry Bundle to log errors

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
+    Sentry\SentryBundle\SentryBundle::class => ['prod' => true],
 ];
