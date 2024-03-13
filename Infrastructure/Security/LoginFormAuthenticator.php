172ffe050e074f4eca3081cd530a1cc6075b0755
Mathieu <mathieu@qizuna.fr>
2024-03-13 14:50:01 +0100

    Starter Kit - security : rollback to loginformauthenticator

--- Infrastructure/Security/LoginFormAuthenticator.php
+++ Infrastructure/Security/LoginFormAuthenticator.php
@@ -13,12 +13,12 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\UserNotFoundException;
-use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
+use Symfony\Component\Security\Http\SecurityRequestAttributes;
 use Symfony\Component\Security\Http\Util\TargetPathTrait;
 
 final class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
@@ -42,7 +42,7 @@ final class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
         /** @var string $csrfToken */
         $csrfToken = $request->request->get('_csrf_token');
 
-        $request->getSession()->set(Security::LAST_USERNAME, $username);
+        $request->getSession()->set(SecurityRequestAttributes::LAST_USERNAME, $username);
 
         return new Passport(
             new UserBadge($username, function ($userIdentifier) {
