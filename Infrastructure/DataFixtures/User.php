80244ccbc0562c05ea159956e9b6d70d05a759e6
Mathieu <mathieu@qizuna.fr>
2024-07-30 06:09:28 +0200

    Starter Kit - multi-tenancy enhancement

--- Infrastructure/DataFixtures/User.php
+++ Infrastructure/DataFixtures/User.php
@@ -10,29 +10,43 @@ use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
+use function get_class;
+
 final class User extends Fixture implements DependentFixtureInterface
 {
-    public function __construct(private UserPasswordHasherInterface $userPasswordHasher)
+    public function __construct(
+        private UserPasswordHasherInterface $userPasswordHasher,
+
+    )
     {
     }
 
     public function load(ObjectManager $manager): void
     {
+        $tenant = $this->getReference('tenant-1');
+
         $manager->persist($this->generateUser('user'));
-        $manager->persist($this->generateUser('admin', true, ["ROLE_ADMIN"]));
+        $manager->persist($this->generateUser('admin', true, ["ROLE_LOGICIEL_ADMINISTRATEUR"]));
+
+        $manager->persist($this->generateUser('user_client', true, ["ROLE_CLIENT_UTILISATEUR"], $tenant));
+        $manager->persist($this->generateUser('admin_client', true, ["ROLE_CLIENT_ADMINISTRATEUR"], $tenant));
 
         $manager->flush();
     }
 
-    private function generateUser(string $username, bool $isActive = true, array $roles = []): \Infrastructure\Entities\User
-    {
+    private function generateUser(
+        string $username,
+        bool $isActive = true,
+        array $roles = [],
+        $tenant = null
+    ): \Infrastructure\Entities\User {
         $user = new \Infrastructure\Entities\User();
         $user->setUsername($username);
         $user->setPassword($this->userPasswordHasher->hashPassword($user, 'password'));
         $user->setEmail("{$username}@email.fr");
         $user->setRoles($roles);
         $user->setCreatedAt(new DateTimeImmutable());
-        $user->setTenant($this->getReference('tenant-1'));
+        $user->setTenant($tenant);
 
         if ($isActive) {
             $user->setActivatedAt(new DateTimeImmutable('2000-01-01'));
@@ -40,6 +54,7 @@ final class User extends Fixture implements DependentFixtureInterface
 
         return $user;
     }
+
 
     public function getDependencies()
     {
